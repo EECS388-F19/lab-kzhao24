28f3294220deda741477b5bbd2a18fd2e4e832f7
Kevin Zhao <zhaokev@umich.edu>
2019-09-09 17:46:00 -0400

    helloworld.py commit

---
+++ helloworld.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from random import randint
+
+print('Kevin')
+x = randint(0,100)
+y = randint(0,100)
+print(x)
+print(y)
+sumXY = x + y
+print("Sum = " + str(sumXY))
+avg = sumXY/2
+print("Average = " + str(avg))
